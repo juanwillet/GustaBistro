76e68970d65b9b7df51c8304f158c54847f8778b
juan willet <juanwillet@gmail.com>
2024-06-05 20:36:30 -0400

    se modificaron estilos en la zona de Nosotros del index

---
+++ js/main.js
@@ -0,0 +1,172 @@
+const sandwichPolloCrispy = {
+    nombre: 'sandwich de pollo crispy',
+    precio: 15
+}
+const gustaBurguer={
+    nombre: 'gustaburguer',
+    precio: 15
+}
+const sweetBaconBurguerguer={
+    nombre: 'sweet bacon burguer',
+    precio: 15
+}
+const gustaChickenSalad={
+    nombre: 'gusta chicken salad',
+    precio: 15
+}
+const gustaMarina={
+    nombre: 'gusta marina',
+    precio: 15
+}
+const mushroomGravyBurguer={
+    nombre: 'mushroom gravy burguer',
+    precio: 15
+}
+const hamburguesas =[sandwichPolloCrispy, gustaBurguer, sweetBaconBurguerguer, gustaChickenSalad, gustaMarina, mushroomGravyBurguer]
+
+const fetucciniAlfredo={
+    nombre: 'fetuccini alfredo',
+    precio: 18
+}
+const amatriciana={
+    nombre: 'amatriciana',
+    precio: 18
+}
+const raviolisSalsaDeTomate={
+    nombre: 'raviolis en salsa de tomate',
+    precio: 18
+}
+const pastaPollo={
+    nombre: 'pasta de pollo',
+    precio: 18
+}
+const pastaPesto={
+    nombre: 'pasta en salsa pesto',
+    precio: 18
+}
+const raviolisEspinaca={
+    nombre: 'raviolis con espinaca',
+    precio: 18
+}
+const pastas= [fetucciniAlfredo, amatriciana, raviolisSalsaDeTomate, pastaPollo, pastaPesto, raviolisEspinaca]
+
+const ingredienteAceitesNegras={
+    nombre: 'aceitunas negras',
+    precio: 3
+}
+const ingredienteJamon={
+    nombre: 'jamon',
+    precio: 3
+}
+const ingredientePeperoni={
+    nombre: 'peperoni',
+    precio: 3
+}
+const ingredienteTocino={
+    nombre: 'tocino',
+    precio: 3
+}
+const ingredienteVegetalesRostizados={
+    nombre: 'vegetales rostizados',
+    precio: 3
+}
+const pizzasIngredientes= [ingredienteAceitesNegras, ingredienteJamon, ingredientePeperoni, ingredienteTocino, ingredienteVegetalesRostizados]
+
+const margaritaTradicional={
+    nombre: 'margarita',
+    precio: 15
+}
+const unIngrediente={
+    nombre: 'un ingrediente',
+    precio: 18
+}
+const dosIngredientes={
+    nombre: 'dos ingredientes',
+    precio: 21
+}
+const tresIngredientes={
+    nombre: 'tres ingredientes',
+    precio: 24
+}
+const cuatroIngredientes={
+    nombre: 'cuatro ingredientes',
+    precio: 27
+}
+const pizzas =[margaritaTradicional, unIngrediente, dosIngredientes, tresIngredientes, cuatroIngredientes]
+
+const sauvignonBlanco={
+    nombre: 'sauvignon blanco',
+    precio: 18
+}
+const rose={
+    nombre: 'rose',
+    precio: 18
+}
+const merlot={
+    nombre: 'merlot',
+    precio: 18
+}
+const cabernetSauvignon={
+    nombre: 'cabernet sauvignon',
+    precio: 18
+}
+const vinoTintoSeco={
+    nombre: 'vino tinto seco',
+    precio: 18
+}
+const vinoTintoBlanco={
+    nombre: 'vino blanco seco',
+    precio: 18
+}
+const vinos= [sauvignonBlanco, rose, merlot, cabernetSauvignon, vinoTintoSeco, vinoTintoBlanco]
+
+const soleraVerde={
+    nombre: 'solera verde',
+    precio: 3,
+    cantidad: 1
+}
+const soleraAzul={
+    nombre: 'solera azul',
+    precio: 3,
+    cantidad: 1
+}
+const corona={
+    nombre: 'corona',
+    precio: 4,
+    cantidad: 1
+}
+const baldeZulia={
+    nombre: 'balde zulia',
+    precio: 18,
+    cantidad: 12
+}
+const baldePolar={
+    nombre: 'balde polar',
+    precio: 18,
+    cantidad: 12
+}
+const heineken={
+    nombre: 'heineken',
+    precio: 5,
+    cantidad: 1
+}
+const cervezas= [soleraVerde, soleraAzul, corona, baldeZulia, baldePolar, heineken]
+
+const margarita={
+    nombre: 'margarita',
+    precio: 10,
+    cantidad: 1
+}
+const mojito={
+    nombre: 'mojito',
+    precio: 8,
+    cantidad: 1
+}
+const negroni={
+    nombre: 'negroni',
+    precio: 10,
+    cantidad: 1 
+}
+const cocteles= [margarita, mojito, negroni]
+
+
